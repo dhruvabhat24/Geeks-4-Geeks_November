34c7b6900f7d3de17ba850e0aebdb1ec2562a84a
Dhruva Bhat S N <dhruvabhat24@gmail.com>
2023-11-19 07:48:29 +0530

    Create 19.java

---
+++ 19.java
@@ -0,0 +1,102 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+class Node 
+{
+    int data;
+    Node next;
+    
+    Node(int a)
+        {
+            data = a;
+            next = null;
+        }
+}
+class GfG
+{
+    static Scanner sc = new Scanner(System.in);
+    public static Node inputList(int size)
+    {
+        Node head, tail;
+        int val;
+        
+        val = sc.nextInt();
+        
+        head = tail = new Node(val);
+        
+        size--;
+        
+        while(size-->0)
+        {
+            val = sc.nextInt();
+            tail.next = new Node(val);
+            tail = tail.next;
+        }
+        
+        return head;
+    }
+    
+    public static void printList(Node n)
+    {
+        while(n!=null)
+        {
+            System.out.print(n.data + " ");
+            n = n.next;
+        }
+    }
+    
+    public static void main(String args[])
+        {
+            
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int n , m;
+                    
+                    n = sc.nextInt();
+                    m = sc.nextInt();
+                    
+                    Node head1 = inputList(n);
+                    Node head2 = inputList(m);
+                    
+                    Solution obj = new Solution();
+                    
+                    Node result = obj.findIntersection(head1, head2);
+	    
+	                printList(result);
+	                System.out.println();
+                }
+        }
+}
+// } Driver Code Ends
+
+
+/* Node of a linked list
+ class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+class Solution
+{
+   public static Node findIntersection(Node head1, Node head2)
+    {
+        // code here.
+        Node dummyNode=new Node(0);
+        Node currNode=dummyNode;
+        while(head1!=null && head2!=null){
+            if(head1.data==head2.data){
+                currNode.next=new Node(head1.data);
+                currNode=currNode.next;
+                head1=head1.next;
+                head2=head2.next;
+            }else if(head1.data<head2.data){
+                head1=head1.next;
+            }else{
+                 head2=head2.next;
+            }
+        }
+        return dummyNode.next;
+    }
+}

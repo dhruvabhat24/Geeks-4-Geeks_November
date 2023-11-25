9cdc4006c29b428a4500fae743c7b023cb3e1f22
Dhruva Bhat S N <dhruvabhat24@gmail.com>
2023-11-25 15:30:09 +0530

    Create 25.java

---
+++ 25.java
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+
+
+
+
+class  Solution
+{
+    void shuffleArray(long arr[], int n)
+    {
+        // Your code goes here
+        int j = n/2 , count = 0;
+        long ls[] = new long[n];
+        for(int i = 0 ; i < n/2 ; i++,j++)
+        {
+            ls[count++] = arr[i];
+            ls[count++] = arr[j];
+        }
+        
+        for(int i = 0 ; i < n ; i++)
+        {
+            arr[i] = ls[i];
+        }
+    }
+}
+
+
+//{ Driver Code Starts.
+
+// Driver class
+class Array {
+
+    // Driver code
+    public static void main(String[] args) throws IOException {
+        // Taking input using buffered reader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testcases = Integer.parseInt(br.readLine());
+        // looping through all testcases
+        while (testcases-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+//            String line = br.readLine();
+//            String[] q = line.trim().split("\\s+");
+//            int n =Integer.parseInt(q[0]);
+//            int k =Integer.parseInt(q[1]);
+            //int y =Integer.parseInt(a2[2]);
+            String line1 = br.readLine();
+            String[] a1 = line1.trim().split("\\s+");
+            long a[] = new long[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(a1[i]);
+            }
+//            String line2 = br.readLine();
+//            String[] a2 = line2.trim().split("\\s+");
+//            long b[] = new long[n];
+//            for (int i = 0; i < m; i++) {
+//                b[i] = Long.parseLong(a2[i]);
+//            }
+             Solution ob = new  Solution();
+            //ArrayList<Long> ans=
+            ob.shuffleArray(a,n);
+           // System.out.println(ob.shuffleArray(a,n));
+            for (int i = 0; i < n; i++)
+                System.out.print(a[i]+" ");
+            System.out.println();
+        }
+    }
+}
+
+
+// } Driver Code Ends
